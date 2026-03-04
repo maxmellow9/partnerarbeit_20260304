55c99a1590baa72a498aee0c9765e5fe99a84130
maxmellow9 <maximiliane.agarwalla@proton.me>
2026-03-04 11:59:19 +0100

    Merge: gemeinsame Lösung

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 def greeting(name: str) -> str:
 <<<<<<< HEAD
  return f"Hello, {name}! Nice to meet you."
-=======
- return f"Good day, {name}!"
->>>>>>> feature-formal
+#=======
+ #return f"Good day, {name}!"
+#>>>>>>> feature-formal
 def main():
  print(greeting("Team"))
 if __name__ == "__main__":
